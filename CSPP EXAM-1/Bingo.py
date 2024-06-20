cc91fbb9e3fcd3823e14bc589f56bba17cfa7018
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-20 22:43:11 +0530

    code modified for displaying grid

--- CSPP EXAM-1/Bingo.py
+++ CSPP EXAM-1/Bingo.py
@@ -1,71 +1,48 @@
 import random
-def empty_Grid(rows, cols):
-    grid = []
+def emptyGrid(rows,cols):
+    grid=[]
     for i in range(rows):
-        row = []
+        s=[]
         for j in range(cols):
-            row.append(None)
-        grid.append(row)
+            s.append("_")
+        grid.append(s)
     return grid
-# rows = 5
-# cols = 5
-# print(empty_Grid(rows, cols))
+# print(emptyGrid(5,5))
 
 
-def populate_grid(grid, numbers):
-    x = set()
-    for num in numbers:
-        while True:
-            row = random.randint(0, 4)
-            col = random.randint(0, 4)
-            if (row, col) not in x:
-                grid[row][col] = num
-                x.add((row, col))
-                break
+# print(numbers)
+def populateGrid(grid,numbers):
+    count=0
+    while count<16:
+        row= random.choice([0,1,2,3,4])
+        col=random.choice([0,1,2,3,4])
+        if grid[row][col]=="_":
+            grid[row][col]=numbers.pop()
+            count+=1
     return grid
+# numbers=random.sample(range(1,25),16)
+# grid=emptyGrid(5,5)
+# print(populateGrid(grid,numbers))
 
-def getUserNumbers():
-    user_numbers = []
-    while len(user_numbers) < 16:
+def getUserNumber():
+    while True:
         try:
-            num = int(input(f"Enter number {len(user_numbers) + 1}/16 (between 1 and 25): "))
-            if num < 1 or num > 25:
-                print("number is not valid.")
-            elif num in user_numbers:
-                print("number already chosen.")
-            else:
-                user_numbers.append(num)
+            num=int(input("enter your number between 1 to 16: "))
+            if 1<=num<=25:
+                return num
         except ValueError:
-            print("Invalid input.")
-    return user_numbers
-# getUserNumbers()
+            print("invalid inputs")
+def generateComputerNumber():
+    num=random.sample(range(1,16),1)
+    return num[0]
 
 
-def generateComputerNumbers():
-    return random.sample(range(1,25),16)
-
-
-def display_Grid(grid):
-    for r in grid:
-        for n in r:
-            if n is not None:
-                print('n',end=' ')
-            else:
-                print('.',end=' ')
-        print()
-    print()
-
-
-def markNumber(grid, number):
-    for i in range(5):
-        for j in range(5):
-            if grid[i][j]==number:
-                grid[i][j]='x'
-    return grid
-
-def checkWinner(grid):
+def displayGrid(grid):
     for row in grid:
         for each in row:
-            if each is not None and each != 'x':
-                return False
-    return True
+            if each!="_":
+                print(each,end="  ")
+            else:
+                print("*",end="  ")
+        print()
+
