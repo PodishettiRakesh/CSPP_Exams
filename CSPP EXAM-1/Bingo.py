a5b314fe27368313bda15e7a3b7ca48a9fa37ce3
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-20 22:35:36 +0530

    function to populate grid with unique random numbers

--- CSPP EXAM-1/Bingo.py
+++ CSPP EXAM-1/Bingo.py
@@ -10,3 +10,16 @@ def empty_Grid(rows, cols):
 # rows = 5
 # cols = 5
 # print(empty_Grid(rows, cols))
+
+
+def populate_grid(grid, numbers):
+    x = set()
+    for num in numbers:
+        while True:
+            row = random.randint(0, 4)
+            col = random.randint(0, 4)
+            if (row, col) not in x:
+                grid[row][col] = num
+                x.add((row, col))
+                break
+    return grid
