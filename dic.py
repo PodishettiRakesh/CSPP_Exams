491834cfd08041fefe9a4d356b8d550ecd749025
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-10 22:57:06 +0530

    function call to print the keys from given dict

--- dic.py
+++ dic.py
@@ -1,9 +1,10 @@
 # def printStudents(dic):
 #     for key,dic in dic.items():
 #         print(key,dic)
-# # dic={"s1":"rakesh","s2":"abhi","s3":"keerthi"}
+dic={"s1":"rakesh","s2":"abhi","s3":"keerthi"}
 # # printStudents(dic)
 
 def printKeys(dic):
     for key in dic.keys():
         print(key)
+printKeys(dic)
