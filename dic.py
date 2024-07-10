ef0381a063866a366ecae3d4cdb264ae42a214bc
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-10 22:56:06 +0530

    function to print the key in given dict

--- dic.py
+++ dic.py
@@ -1,5 +1,8 @@
-def printStudents(dic):
-    for key,dic in dic.items():
-        print(key,dic)
-# dic={"s1":"rakesh","s2":"abhi","s3":"keerthi"}
-# printStudents(dic)
+# def printStudents(dic):
+#     for key,dic in dic.items():
+#         print(key,dic)
+# # dic={"s1":"rakesh","s2":"abhi","s3":"keerthi"}
+# # printStudents(dic)
+
+def printKeys(dic):
+    
