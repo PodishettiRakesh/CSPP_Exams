20024e34ea327df550fc4713cf23e4836c73d7e9
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-10 22:57:52 +0530

    created fun to print the values of dict

--- dic.py
+++ dic.py
@@ -1,10 +1,13 @@
-# def printStudents(dic):
-#     for key,dic in dic.items():
-#         print(key,dic)
+def printStudents(dic):
+    for key,dic in dic.items():
+        print(key,dic)
 dic={"s1":"rakesh","s2":"abhi","s3":"keerthi"}
-# # printStudents(dic)
+printStudents(dic)
 
 def printKeys(dic):
     for key in dic.keys():
         print(key)
 printKeys(dic)
+
+def printValues(dic):
+    
